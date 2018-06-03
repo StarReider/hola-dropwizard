aba894b063065479dc6af2c64ee7827bb3ea956c
Roman <Roman@netcracker>
2018-06-03 18:46:59 +0300

    Initial commit

---
+++ holaspringboot/src/main/java/com/redhat/examples/holaspringboot/HolaspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.examples.holaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaspringbootApplication.class, args);
+	}
+}
